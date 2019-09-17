72df1467f0708775053617ca1326bcd7f2561534
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-09-17 12:24:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,6 @@
-function showRepositories() {
-  var repos = JSON.parse(this.responseText);
-  console.log(repos);
-  const repoList = `<ul>${repos
-    .map(
-      r =>
-        '<li>' +
-        r.title +
-        ' - <a href="#" data-repo="' +
-        r.title +
-        '" onclick="getCommits(this)">Get Commits</a></li>'
-    )
-    .join('')}</ul>`;
-  document.getElementById('repositories').innerHTML = repoList;
-}
 
 function getRepositories() {
   const req = new XMLHttpRequest()
-  req.addEventListener('load', 'showRepositories')
   req.open('GET', 'https://ghibliapi.herokuapp.com/films')
   req.send()
 }
