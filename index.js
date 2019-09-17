66ce53b3b3a7b66ddc1e6d3e56c1344f9c053b43
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-09-17 13:21:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function getRepositories(){
   var request = new XMLHttpRequest();
-  
+
   request.open('GET', 'https://tv-v2.api-fetch.website/movie/{imdb_id}');
-  
+
   request.onreadystatechange = function () {
     if (this.readyState === 4) {
       console.log('Status:', this.status);
@@ -10,5 +10,5 @@ function getRepositories(){
       console.log('Body:', this.responseText);
     }
   };
-  
+
   request.send();
