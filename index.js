60c23d6850a09faa713969fbbf08180c33252fd4
nawaar.zakee <nawaar.zakee@yahoo.com>
2019-09-17 13:12:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getRepositories(){
   const req = new XMLHttpRequest()
-  req.open('get', 'https://ghibliapi.herokuapp.com/films', true)
+  req.open('get', 'https://github.com/popcorn-official/popcorn-api.git', true)
   req.send()
 }
